5ef987a47f5476a36d22fd4d77308d5933b8af5d
Mohammad rezai <mohammadhossein.rezai.66@gmail.com.com>
2024-02-07 04:07:48 -0800

    Added loading screen during page load

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import HomePage from "./Page/Home/HomePage";
+import { metronome } from "ldrs";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  metronome.register();
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <div className="App">
-      <HomePage />
+      {loading ? (
+        <div className="loading">
+          <l-metronome size="45" speed="1.75" color="black"></l-metronome>
+        </div>
+      ) : (
+        <HomePage />
+      )}
     </div>
   );
 }

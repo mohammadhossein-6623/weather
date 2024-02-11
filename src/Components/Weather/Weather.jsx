2dc263f6568443727f911c9204dc6a6b076f4c18
Mohammad rezai <mohammadhossein.rezai.66@gmail.com.com>
2024-02-11 07:19:14 -0800

    add useWeather

--- src/Components/Weather/Weather.jsx
+++ src/Components/Weather/Weather.jsx
@@ -1,40 +1,17 @@
 import React, { useState } from "react";
 import "./weather.css";
-import apiRequests from "../../services/Axios/Configs";
+import useWeather from "../../Hooks/useWeather";
 import WeatherDisplay from "../WeatherDisplay/WeatherDisplay";
 import Input from "../Input/Input";
 
 export default function Weather() {
   const [onSearch, setOnSearch] = useState("");
-  const [weather, setWeather] = useState(null);
-  const [errorMessage, setErrorMessage] = useState("");
-
-  const getWeatherData = async (cityName) => {
-    try {
-      const res = await apiRequests.get("/data/2.5/weather", {
-        params: {
-          q: cityName,
-          appid: "08b53885fe5c4b4a1856ce0d6d77038c",
-        },
-      });
-
-      if (res.data.cod !== 200) {
-        setErrorMessage(res.data.message);
-        setWeather(null);
-      } else {
-        setWeather(res.data);
-        setErrorMessage("");
-      }
-      setOnSearch("");
-    } catch (error) {
-      setErrorMessage("Really? Write the name of a city!!");
-      console.error("Error fetching weather data:", error);
-    }
-  };
+  const { weather, errorMessage, getWeatherData } = useWeather();
 
   const inputHandler = (event) => {
     if (event.key === "Enter") {
       getWeatherData(onSearch);
+      setOnSearch(""); // Move this inside the custom hook
     }
   };
 
---
+++ src/Hooks/useWeather.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import apiRequests from "../services/Axios/Configs";
+
+const useWeather = () => {
+  const [weather, setWeather] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const getWeatherData = async (cityName) => {
+    try {
+      const res = await apiRequests.get("/data/2.5/weather", {
+        params: {
+          q: cityName,
+          appid: "08b53885fe5c4b4a1856ce0d6d77038c",
+        },
+      });
+
+      if (res.data.cod !== 200) {
+        setErrorMessage(res.data.message);
+        setWeather(null);
+      } else {
+        setWeather(res.data);
+        setErrorMessage("");
+      }
+    } catch (error) {
+      setErrorMessage("Really? Write the name of a city!!");
+      console.error("Error fetching weather data:", error);
+    }
+  };
+
+  return { weather, errorMessage, getWeatherData };
+};
+
+export default useWeather;
